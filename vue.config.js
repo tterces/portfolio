30ccd8359caf856609e4aa5d92d741df6f6be461
YS <dldudtjr6111@gmail.com>
2021-01-14 04:23:12 +0000

    host & port

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+	configureWebpack: {
+
+         devServer: {
+
+             host: '0.0.0.0',
+
+             port: '8080',
+			 disableHostCheck: true
+         }
+
+    }
 }
